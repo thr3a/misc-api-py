dff47430b871fc8d6ab178c10675b8c22bf14056
thr3a <thr3a@example.com>
2026-01-07 02:11:26 +0900

    update: CORS

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routers import html2md, items, roleplay
 
 app = FastAPI()
+
+# CORSミドルウェアの設定
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://192.168.16.12:5173",
+        "http://localhost:5173",
+        "https://turai.work",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],  # すべてのHTTPメソッドを許可
+    allow_headers=["*"],  # すべてのヘッダーを許可
+)
 
 app.include_router(items.router)
 app.include_router(html2md.router)
